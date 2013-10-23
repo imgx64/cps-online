fb919c3e2360f30ce940ef837ef05a3caac425c1
Ibrahim M. Ghazal <imgx64@gmail.com>
2013-10-23 07:59:49 +0300

    Add arrow key navigation to marks

--- static/js/cps.js
+++ static/js/cps.js
@@ -17,6 +17,31 @@
 			$(this).toggleClass("active");
 			$("#" + $(this).data("subject") + "-details").toggle();
 		});
+
+		$(".cps-grid").keydown(function(e) {
+			var col = $(this).closest("td").index()
+
+			switch(e.which) {
+			case 37: // left
+				$(this).closest("td").prevAll().find(".cps-grid").not("[disabled]").last().focus()
+				break;
+
+			case 38: // up
+				$(this).closest("tr").prev().children().eq(col).find(".cps-grid").focus()
+				break;
+
+			case 39: // right
+				$(this).closest("td").nextAll().find(".cps-grid").not("[disabled]").first().focus()
+				break;
+
+			case 40: // down
+				$(this).closest("tr").next().children().eq(col).find(".cps-grid").focus()
+				break;
+
+			default: return; // exit this handler for other keys
+			}
+			e.preventDefault(); // prevent the default action (scroll / move caret)
+		});
 	});
 
 })(window.jQuery);
