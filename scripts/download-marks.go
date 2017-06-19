2c766d3a95b680cefd27eaed2dd81b0ff08597a5
Ibrahim M. Ghazal <imgx64@gmail.com>
2017-06-19 14:37:36 +0300

    Make download-marks script interactive

--- scripts/download-marks.go
+++ scripts/download-marks.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -11,80 +13,23 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-var cookieValue = "REPLACE"
-
-var subjects = []string{
-	"English",
-	"Speech and Drama",
-	"Math",
-	"Social Studies",
-	"Science",
-	"Computer",
-	"Arabic",
-	"Citizenship",
-	"Social Studies Arabic",
-	"UCMAS",
-	"Islamic Studies",
-	"PE",
-	"Biology",
-	"Physics",
-	"Chemistry",
-	"Economics",
-	"Accounting",
-	"Business Studies",
-	"Environmental Science",
-	"Ecology",
-	"Arabic 102",
-	"Arabic 201",
-	"Arabic 101",
-	"Arabic 202",
-	"Arabic 301",
-	"Arabic 302",
-	"Islamic Studies 101",
-	"Islamic Studies 301",
-	"Islamic Studies 201",
-	"Islamic Studies 103",
-	"Islamic Studies 104",
-}
-
-var extraSubjects = []string{
-	"Remarks",
-	"Behavior",
-}
-
-var classes = []struct {
-	class      string
-	maxSection rune
-}{
-	{"1A", 'A'},
-	{"1", 'D'},
-	{"2", 'D'},
-	{"3", 'D'},
-	{"4", 'C'},
-	{"5", 'B'},
-	{"6", 'C'},
-	{"7", 'B'},
-	{"8", 'B'},
-	{"9", 'A'},
-	{"9SCI", 'B'},
-	{"9COM", 'B'},
-	{"10SCI", 'B'},
-	{"10COM", 'B'},
-	{"11SCI", 'A'},
-	{"11COM", 'A'},
-	{"12SCI", 'A'},
-	{"12COM", 'A'},
-	{"PREKG", 'A'},
-	{"KG1", 'C'},
-	{"KG2", 'D'},
-	{"SN", 'A'},
+func parseClassSection(classSection string) (class, section string, err error) {
+	cs := strings.Split(classSection, "|")
+	if len(cs) != 2 {
+		return "", "", fmt.Errorf("Unable to parse class and section: %s", classSection)
+	}
+	class = cs[0]
+	section = cs[1]
+	return class, section, nil
 }
 
 // Copied from gradingsystems.go
+
 type termType int
 
 const (
@@ -92,6 +37,9 @@ const (
 	Semester
 	EndOfYear
 	EndOfYearGpa
+	Midterm
+	WeekS1
+	WeekS2
 )
 
 var termStrings = map[termType]string{
@@ -99,21 +47,14 @@ var termStrings = map[termType]string{
 	Semester:     "Semester",
 	EndOfYear:    "End of Year",
 	EndOfYearGpa: "End of Year (GPA)",
+	Midterm:      "Midterm",
+	WeekS1:       "S1 Week",
+	WeekS2:       "S2 Week",
 }
 
 type Term struct {
 	Typ termType
 	N   int
-}
-
-var terms = []Term{
-	{Quarter, 1},
-	{Quarter, 2},
-	{Semester, 1},
-	{Quarter, 3},
-	{Quarter, 4},
-	{Semester, 2},
-	{EndOfYear, 0},
 }
 
 func parseTerm(s string) (Term, error) {
@@ -145,6 +86,11 @@ func (t Term) Value() string {
 	return fmt.Sprintf("%d|%d", t.Typ, t.N)
 }
 
+// Used in reportcards template
+func (t Term) ShowBehaviorReportCard() bool {
+	return t.Typ == Quarter || t.Typ == Midterm
+}
+
 func (t Term) String() string {
 	s, ok := termStrings[t.Typ]
 	if !ok {
@@ -158,8 +104,161 @@ func (t Term) String() string {
 
 // End - Copied from gradingsystems.go
 
+type Terms []Term
+
+func (t Terms) Len() int {
+	return len(t)
+}
+
+func (t Terms) Less(i, j int) bool {
+	t1 := t[i]
+	t2 := t[j]
+
+	if t1.Typ == t2.Typ {
+		return t1.N < t2.N
+	}
+
+	if t1.Typ == WeekS1 {
+		return true
+	}
+	if t2.Typ == WeekS1 {
+		return false
+	}
+
+	if t1.Typ == Quarter && t1.N == 1 {
+		return true
+	}
+	if t2.Typ == Quarter && t2.N == 1 {
+		return false
+	}
+
+	if t1.Typ == Midterm && t1.N == 1 {
+		return true
+	}
+	if t2.Typ == Midterm && t2.N == 1 {
+		return false
+	}
+
+	if t1.Typ == Quarter && t1.N == 2 {
+		return true
+	}
+	if t2.Typ == Quarter && t2.N == 2 {
+		return false
+	}
+
+	if t1.Typ == Semester && t1.N == 1 {
+		return true
+	}
+	if t2.Typ == Semester && t2.N == 1 {
+		return false
+	}
+
+	if t1.Typ == WeekS2 {
+		return true
+	}
+	if t2.Typ == WeekS2 {
+		return false
+	}
+
+	if t1.Typ == Quarter && t1.N == 3 {
+		return true
+	}
+	if t2.Typ == Quarter && t2.N == 3 {
+		return false
+	}
+
+	if t1.Typ == Midterm && t1.N == 2 {
+		return true
+	}
+	if t2.Typ == Midterm && t2.N == 2 {
+		return false
+	}
+
+	if t1.Typ == Quarter && t1.N == 4 {
+		return true
+	}
+	if t2.Typ == Quarter && t2.N == 4 {
+		return false
+	}
+
+	if t1.Typ == Semester && t1.N == 2 {
+		return true
+	}
+	if t2.Typ == Semester && t2.N == 2 {
+		return false
+	}
+
+	if t1.Typ == Quarter {
+		return true
+	}
+	if t2.Typ == Quarter {
+		return false
+	}
+
+	if t1.Typ == Midterm {
+		return true
+	}
+	if t2.Typ == Midterm {
+		return false
+	}
+
+	if t1.Typ == Semester {
+		return true
+	}
+	if t2.Typ == Semester {
+		return false
+	}
+
+	if t1.Typ == EndOfYear {
+		return true
+	}
+	if t2.Typ == EndOfYear {
+		return false
+	}
+
+	if t1.Typ == EndOfYearGpa {
+		return true
+	}
+	if t2.Typ == EndOfYearGpa {
+		return false
+	}
+
+	panic(fmt.Sprintf("Invalid terms: %s %s", t1, t2))
+
+}
+
+func (t Terms) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
 func main() {
 	var err error
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+		fmt.Println("Press Enter to close")
+		fmt.Scanln()
+	}()
+
+	fmt.Print("Enter cookie value: ")
+	var rawCookies string
+	if _, err = fmt.Scanln(&rawCookies); err != nil {
+		panic(err)
+	}
+
+	rawCookies = strings.Trim(rawCookies, " \t\n")
+	rawCookies = strings.TrimPrefix(rawCookies, "cookie:")
+
+	rawRequest := fmt.Sprintf("GET / HTTP/1.0\r\nCookie: %s\r\n\r\n", rawCookies)
+	req, err := http.ReadRequest(bufio.NewReader(strings.NewReader(rawRequest)))
+	if err != nil {
+		panic(err)
+	}
+	cookies := req.Cookies()
+
 	http.DefaultClient.Jar, err = cookiejar.New(nil)
 	if err != nil {
 		panic(err)
@@ -169,49 +268,129 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	http.DefaultClient.Jar.SetCookies(baseUrl, []*http.Cookie{
-		{
-			Name:  "SACSID",
-			Value: cookieValue,
-			Path:     "/",
-			Secure:   true,
-			HttpOnly: true,
-		},
-	})
+	http.DefaultClient.Jar.SetCookies(baseUrl, cookies)
 
 	http.DefaultClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
 		if req.URL.Host != via[len(via)-1].URL.Host {
-			return fmt.Errorf("Redirecting to new host: %s", req.URL.Host)
+			return fmt.Errorf("Invalid cookie. Redirecting to new host: %s", req.URL.Host)
 		}
 		return nil
 	}
 
-	var allSubjects []string
-	allSubjects = append(allSubjects, subjects...)
-	allSubjects = append(allSubjects, extraSubjects...)
+	subjectsMapUrl, err := url.Parse("https://creativity-private-school-2015.appspot.com/subjectsmap")
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Println("Getting terms, classes, and subjects...")
+	resp, err := http.Get(subjectsMapUrl.String())
+	if err != nil {
+		panic(err)
+	}
+
+	if resp.StatusCode != 200 {
+		panic(resp)
+	}
+
+	// term -> class -> subject
+	subjectsMap := make(map[string]map[string][]string)
+	if err := json.NewDecoder(resp.Body).Decode(&subjectsMap); err != nil {
+		panic(err)
+	}
+
+	resp.Body.Close()
+
+	var terms Terms
+	for termValue, _ := range subjectsMap {
+		term, err := parseTerm(termValue)
+		if err != nil {
+			panic(fmt.Sprintf("Invalid term: %s", termValue))
+		}
+		terms = append(terms, term)
+	}
+
+	sort.Sort(terms)
+
+	fmt.Println("Select term to download")
+	i := 0
 	for _, term := range terms {
-		for _, class := range classes {
-			if class.maxSection < 'A' ||
-				class.maxSection > 'Z' {
-				fmt.Printf("Invalid maxSection: %#v", class)
-			}
+		i++
+		fmt.Printf("%d) %s\n", i, term)
+	}
+	fmt.Println("*) All terms")
 
-			for section := 'A'; section <= class.maxSection; section++ {
-				for _, subject := range allSubjects {
-					download(term, class.class, section, subject)
-				}
+	var allTerms bool
+	var termIndex int
+	for {
+		fmt.Print("Type number or *: ")
+		var termInput string
+		if _, err = fmt.Scanln(&termInput); err != nil {
+			continue
+		}
+		termInput = strings.Trim(termInput, " \t\n")
+		if termInput == "*" {
+			allTerms = true
+			termIndex = -1
+			break
+		}
+
+		if termNumber, err := strconv.Atoi(termInput); err == nil {
+			if termNumber >= 1 && termNumber <= len(terms) {
+				allTerms = false
+				termIndex = termNumber - 1
+				break
 			}
+		}
+
+		fmt.Println("Invalid input")
+	}
+
+	if !allTerms {
+		terms = []Term{terms[termIndex]}
+	}
+
+	fmt.Println("Downloading marks...")
+
+	total := 0
+	for _, term := range terms {
+		termMap := subjectsMap[term.Value()]
+		for _, subjects := range termMap {
+			total = total + len(subjects)
 		}
 	}
 
+	i = 1
+	for _, term := range terms {
+		termMap := subjectsMap[term.Value()]
+
+		var classes []string
+		for classSection, _ := range termMap {
+			classes = append(classes, classSection)
+		}
+		sort.Strings(classes)
+
+		for _, classSection := range classes {
+			subjects := termMap[classSection]
+			class, section, err := parseClassSection(classSection)
+			if err != nil {
+				panic(err)
+			}
+			for _, subject := range subjects {
+				fmt.Printf("(%4d/%4d) ", i, total)
+				download(term, class, section, subject)
+				i++
+			}
+		}
+	}
+	fmt.Println("Done")
+
 }
 
-func download(term Term, class string, section rune, subject string) {
-	id := fmt.Sprintf("%15s %7s %c %22s", term, class, section, subject)
+func download(term Term, class, section, subject string) {
+	id := fmt.Sprintf("%15s %7s %s %22s", term, class, section, subject)
 
 	filedir := filepath.Join(".", "Marks", term.String(), subject)
-	filename := fmt.Sprintf("%s-%s%c.csv", subject, class, section)
+	filename := fmt.Sprintf("%s-%s%s.csv", subject, class, section)
 	file := filepath.Join(filedir, filename)
 
 	_, err := os.Stat(file)
@@ -227,7 +406,7 @@ func download(term Term, class string, section rune, subject string) {
 
 	query := downloadUrl.Query()
 	query.Set("Term", term.Value())
-	query.Set("ClassSection", fmt.Sprintf("%s|%c", class, section))
+	query.Set("ClassSection", fmt.Sprintf("%s|%s", class, section))
 	query.Set("Subject", subject)
 
 	downloadUrl.RawQuery = query.Encode()
